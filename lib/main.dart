4ea1648f5284207c9d73066e0a89fa6c44e2f6f2
aarushi-v <aarushi.vx@gmail.com>
2023-03-28 16:44:33 -0700

    changes from march 28

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
@@ -70,10 +70,23 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('My App'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // Do something when the user taps the settings button
+            },
+          ),
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            // Open the navigation drawer
+          },
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
